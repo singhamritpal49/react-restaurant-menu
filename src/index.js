ba196527a02fd909c2898cb35f772bd843ec2bd8
Amritpal Singh <singhamritpal49@gmail.com>
2019-12-28 01:51:19 -0500

    created menu app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+class StorePicker extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+}
+
+export default StorePicker;
